09fc6103499c6a09791f083d94613625eaf6a8a6
Rodolfo Diaz <rodolfodzr@gmail.com>
2021-02-15 21:38:07 -0500

    configuring side navbar and collapsing menu

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,3 +1,4 @@
+import { Dropdown } from 'bootstrap'
 import React from 'react'
 import SideItem from "../SideItem"
 
@@ -6,8 +7,15 @@ const Wrapper = (props) => {
         <div className="container principal">
             <div className="col-sm-2">
             {/* adding a collapsable button  */}
-                <a href="#" data-activates="slide-out" className="btn btn-dark p-2 button-collapse"><i className="fas fa-bars fa-2x"></i></a> 
-                <SideItem />
+            <div className="dropdown">
+            {/* <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button> */}
+            <a href="#" data-activates="slide-out" className="btn btn-dark p-2 dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><i className="fas fa-bars fa-2x"></i></a> 
+            <SideItem />
+
+            </div>
+                
             </div>
             <div className="col-sm-10">
                 {props.children}
